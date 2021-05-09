3c1c2115b1ddaae149dc45cc3267e822bf23ffd2
Matthew Yacavone <matthew@yacavone.net>
2021-05-09 14:58:20 -0400

    add favicons, build to dist directory

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
+# Clear out the `dist` directory
+rm -rf dist
+mkdir dist
+
+# Move the source files
+cp *.js dist
+cp *.html dist
+cp *.css dist
+cp *.ttf dist
+cp favicons/* dist
+cp .nojekyll dist
+
+# Build microtonal-utils and move its dist
 cd microtonal-utils
 npm install && npm run build:all
 cd ..
-rm -r microtonal-utils-dist
-mv microtonal-utils/dist microtonal-utils-dist
+mkdir dist/microtonal-utils
+cp -R microtonal-utils/dist dist/microtonal-utils/dist
+
+# Move scale-workshop sources
+mkdir -p dist/scale-workshop/src
+cp -R scale-workshop/src/js dist/scale-workshop/src/js
+cp -R scale-workshop/src/lib dist/scale-workshop/src/lib
